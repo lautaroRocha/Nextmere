c8af2d812257ddbcfd4a2235cbe23f5220b5673e
Lautaro Rocha <lautarovrocha@gmail.com>
2023-04-11 16:48:21 -0300

    git commit changed 'products' for 'books'

--- src/app/read/components/WishCounter.tsx
+++ src/app/read/components/WishCounter.tsx
@@ -2,12 +2,10 @@
 
 import { add, remove, reset } from "../../../redux/features/wishesSlice";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import BookMin from "../../products/components/BookMin";
+import BookMin from "../../books/components/BookMin";
 
 export default function WishCounter(){
     const count = useAppSelector((state) => state.wishesReducer.value);
-    const dispatch = useAppDispatch();
-
     return(
         <>
         <span>{count.length}</span>
